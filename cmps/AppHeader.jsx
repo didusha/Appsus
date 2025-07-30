547b98b6f031314a2db804951577699057a49fda
avihayrevivo <avihayr5@gmail.com>
2025-07-30 21:51:53 +0300

    Change logo color

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -4,7 +4,14 @@ export function AppHeader() {
 
     return <header className="app-header">
         <Link to="/">
-            <h3>LOGO ✨</h3>
+            <h3 className="logo">
+                <span className="a">A</span>
+                <span className="p1">p</span>
+                <span className="p2">p</span>
+                <span className="s1">s</span>
+                <span className="u">u</span>
+                <span className="s2">s</span>
+            </h3>
         </Link>
         <nav>
             <NavLink to="/">Home</NavLink>
