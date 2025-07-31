c568d7f14165edd12cfd70cabefa566923df6cd1
Adi <dihash@gmail.com>
2025-07-31 13:08:54 +0300

    Fix CSS

--- apps/mail/cmps/MailFolderList.jsx
+++ apps/mail/cmps/MailFolderList.jsx
@@ -7,7 +7,7 @@ export function MailFolderList({ unReadMails, onClickedSent }) {
     return (
         <section className="mail-folder-list">
             <p><Link className="btn-add-mail" to="/mail/edit" >Compose</Link></p>
-            <p><i className="fa-solid fa-inbox"></i> Inbox <span classNameName="unReadMails" onClick={onClickedSent}>{unReadMails}</span></p>
+            <p><i className="fa-solid fa-inbox"></i> Inbox <span className="unReadMails" onClick={onClickedSent}>{unReadMails}</span></p>
             <p><i className="fa-regular fa-star"></i> Starred <span></span></p>
             <p><i className="fa-regular fa-pen-to-square"></i> Drafts <span></span></p>
             <p><i className="fa-regular fa-paper-plane"></i> Sent <span></span></p>
