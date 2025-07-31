fb59d8b413c398c2897e32a454baac48fb50797b
avihayrevivo <avihayr5@gmail.com>
2025-07-31 18:40:19 +0300

    Edit all notes

--- apps/note/cmps/NoteTodosPreview.jsx
+++ apps/note/cmps/NoteTodosPreview.jsx
@@ -1,17 +1,49 @@
-
-const {useState} = React
+import { NoteEdit } from "../cmps/NoteEdit.jsx"
+const { useState } = React
 
 export function NoteTodosPreview({ note }) {
+    const [todos, setTodos] = useState(note.info.todos || [])
+    const [newTodo, setNewTodo] = useState('')
+    const [isNoteEdit, setIsNoteEdit] = useState(false)
+    const [noteText, setNoteText] = useState(note.info.title)
 
-    const [todos, setTodos] = useState(null)
+
+    function handleAddTodo() {
+        if (!newTodo.trim()) return
+        const updatedTodos = [...todos, { txt: newTodo }]
+        setTodos(updatedTodos)
+        setNewTodo('')
+    }
+
+    function handleRemoveTodo(idxToRemove) {
+        const updatedTodos = todos.filter((_, idx) => idx !== idxToRemove)
+        setTodos(updatedTodos)
+    }
 
     return (
         <article className="note-todos-preview">
-            <ul className="todo-title">{note.info.title}</ul>
-            {note.info.todos.map((todo,idx) => 
-                <li key={idx}>{todo.txt}</li>
+            <h4 className="todo-title" onClick={() => setIsNoteEdit(true)}>{noteText}</h4>
+            <ul>
+                {todos.map((todo, idx) =>
+                    <li key={idx} className="todo-container">
+                        <span className="todo" >{todo.txt}</span>
+                        <button className="remove-todo" onClick={() => handleRemoveTodo(idx)}>x</button>
+                    </li>
+                )}
+            </ul>
+            {isNoteEdit && (
+                <NoteEdit setIsNoteEdit={setIsNoteEdit} noteText={noteText} setNoteText={setNoteText} />
             )}
-            <input type="text" className="todos-input" placeholder="add a task..."/>
+
+            <div className="todo-container">
+                <input
+                    type="text"
+                    className="todos-input"
+                    placeholder="add a task..."
+                    value={newTodo}
+                    onChange={(ev) => setNewTodo(ev.target.value)} />
+                <button className="add-todo" onClick={handleAddTodo}>add</button>
+            </div>
         </article>
     )
 }
