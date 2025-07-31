3e39e66e42a21e6d101d4520e095f5f52459cea4
avihayrevivo <avihayr5@gmail.com>
2025-07-31 11:56:01 +0300

    Change img title

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -17,7 +17,7 @@ export function NoteList({ notes, onRemoveNote, setIsNoteEdit, onSaveColor, onTo
                             <i className="fa-solid fa-thumbtack"></i>
                         </button>
 
-                        <div>
+                        <div className="dynamic-cmp">
                             <DynamicCmp cmpType={note.type} note={note} setIsNoteEdit={setIsNoteEdit} />
                         </div>
                         <div className="note-actions">
