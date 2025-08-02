b737f5c4dc754b60e8844f183e4b927d5e4a21a6
avihayrevivo <avihayr5@gmail.com>
2025-08-02 17:23:40 +0300

    Design about page

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,6 +1,8 @@
 const { Link, NavLink } = ReactRouterDOM
+const { useState } = React
 
 export function AppHeader() {
+    const [isOpen, setIsOpen] = useState(false)
 
     return <header className="app-header">
         <Link to="/">
@@ -13,7 +15,11 @@ export function AppHeader() {
                 <span className="s2">s</span>
             </h3>
         </Link>
-        <nav>
+        <div>
+        <div className="hamburger">
+            <img className="logos" onClick={() => setIsOpen(!isOpen)} src="../gallery/hamburger.svg" alt="" />
+        </div>
+        <nav className={`nav-logos ${isOpen ? 'open' : ''}`}>
             <NavLink to="/" >
                 <img className="logos" src="../gallery/home.svg" alt="" />
             </NavLink>
@@ -27,6 +33,7 @@ export function AppHeader() {
                 <img className="logos" src="../gallery/keep.svg" alt="" />
             </NavLink>
         </nav>
+        </div>
     </header>
 }
 
